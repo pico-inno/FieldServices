e2999b6ce6cfd7d7be658d954cc03a70350b5b74
WaiYanPhoneAant <wypa07@gmail.com>
2023-08-11 14:43:32 +0630

    remove module 8/11

--- Modules/OrderDisplay/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('orderdisplay::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('orderdisplay.name') !!}
-    </p>
-@endsection
