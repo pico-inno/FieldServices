363e486d975143ea9ac487a7a85a5079176d7b6f
WaiYanPhoneAant <wypa07@gmail.com>
2023-08-27 03:23:06 +0630

    remove module

--- Modules/Reservation/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('reservation::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('reservation.name') !!}
-    </p>
-@endsection
