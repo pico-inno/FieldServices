83f62a5943b0c87679213aebed3917b6d9afe383
WaiYanPhoneAant <wypa07@gmail.com>
2023-09-27 14:36:52 +0630

    pos employee suggestion

--- resources/views/App/restaurants/pos/create.blade.php
+++ resources/views/App/restaurants/pos/create.blade.php
@@ -83,7 +83,13 @@
     tagify = new Tagify(employeeInput, {
         whitelist: employee,
         placeholder: "Type something",
-        enforceWhitelist: true
+        enforceWhitelist: true,
+        dropdown: {
+            maxItems: 20,           // <- mixumum allowed rendered suggestions
+            classname: "tags-look", // <- custom classname for this dropdown, so it could be targeted
+            enabled: 0,             // <- show suggestions on focus
+            closeOnSelect: false    // <- do not hide the suggestions dropdown once an item has been selected
+        }
     });
 
 
