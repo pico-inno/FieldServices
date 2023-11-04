2ee68813c1f05681d53bd05da2d21ea545720d51
waiyanphoneaant <wypa07@gmail.com>
2023-11-04 13:23:44 +0630

    remove parent location require

--- resources/views/App/businessSetting/location/edit.blade.php
+++ resources/views/App/businessSetting/location/edit.blade.php
@@ -73,7 +73,7 @@
                                 class="fv-row col-12 col-md-6 d-flex pe-lg-19 mb-10 mt-3 justify-content-between align-items-end">
                                 <!--begin::Label-->
                                 <div class="">
-                                    <label class="required fs-6 fw-semibold mb-2">Parent Location Name</label>
+                                    <label class=" fs-6 fw-semibold mb-2">Parent Location Name</label>
                                 </div>
                                 <!--end::Label-->
                                 <!--begin::Input-->
