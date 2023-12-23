9c6e7fdfa0b1f5e61999b9c4332177edd6e0df5b
waiyanphoneaant <wypa07@gmail.com>
2023-12-23 16:51:18 +0630

    nav ctrl

--- resources/views/App/main/navBar.blade.php
+++ resources/views/App/main/navBar.blade.php
@@ -2292,14 +2292,16 @@
                                                     </a>
                                                 </div>
 
-                                                <div class="menu-item menu-accordion ">
-                                                    <a class="menu-link @yield('games_active')" href="{{route('games.index')}}">
-                                                        <span class="menu-icon">
-                                                            <i class="fa-solid fa-gamepad fs-6"></i>
-                                                        </span>
-                                                        <span class="menu-title">Game</span>
-                                                    </a>
-                                                </div>
+                                                @if(hasModule('games') && isEnableModule('games'))
+                                                    <div class="menu-item menu-accordion ">
+                                                        <a class="menu-link @yield('games_active')" href="{{route('games.index')}}">
+                                                            <span class="menu-icon">
+                                                                <i class="fa-solid fa-gamepad fs-6"></i>
+                                                            </span>
+                                                            <span class="menu-title">Game</span>
+                                                        </a>
+                                                    </div>
+                                                @endif
                                             </div>
                                             <!--end::Wrapper-->
                                         </div>
