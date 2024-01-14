eee6a47c47cbcc8aaee88a5c033e1a21e643587c
paingsoeko <social.kopaing@gmail.com>
2024-01-14 12:15:40 +0630

    remove combokit module

--- Modules/ComboKit/Config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'name' => 'ComboKit',
-    'version'=>'1.0.0'
-];
--- Modules/ComboKit/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('combokit::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('combokit.name') !!}
-    </p>
-@endsection
