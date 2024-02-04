387c0932c500dd6dbc0e591016b3bb445d22dcd5
paingsoeko <social.kopaing@gmail.com>
2024-02-04 16:40:54 +0630

    chage varchar to decemal price column in product_variable table

---
+++ database/migrations/2024_02_04_161251_update_proudct_variable.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+
+    public function up(): void
+    {
+        DB::statement('UPDATE product_variations SET default_purchase_price = REPLACE(default_purchase_price, ",", "")');
+        DB::statement('UPDATE product_variations SET default_selling_price = REPLACE(default_selling_price, ",", "")');
+
+
+
+        Schema::table('product_variations', function (Blueprint $table) {
+            $table->decimal('default_purchase_price', 22,4)->nullable()->change();
+            $table->decimal('profit_percent', 22,4)->nullable()->change();
+            $table->decimal('default_selling_price', 22,4)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
