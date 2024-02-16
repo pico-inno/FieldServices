1541a436f2500e3f19ec8985c0d3012a10f51f31
waiyanphoneaant <wypa07@gmail.com>
2024-02-16 11:38:28 +0630

    query prepare for aung

--- database/migrations/2023_03_24_082352_create_business_users_table.php
+++ database/migrations/2023_03_24_082352_create_business_users_table.php
@@ -13,20 +13,20 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('business_users', function (Blueprint $table) {
-            $table->id();
-            $table->string('username')->unique();
-            $table->integer('role_id');
-            $table->integer('business_id');
-            $table->integer('default_location_id');
-            $table->text('access_location_ids');
-            $table->string('email')->unique()->nullable();
-            $table->string('password');
-            $table->integer('personal_info_id');
-            $table->boolean('is_active')->default(1);
-            $table->string('remember_token', 100)->nullable();
-            $table->timestamps();
-        });
+        // Schema::create('business_users', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->string('username')->unique();
+        //     $table->integer('role_id');
+        //     $table->integer('business_id');
+        //     $table->integer('default_location_id');
+        //     $table->text('access_location_ids');
+        //     $table->string('email')->unique()->nullable();
+        //     $table->string('password');
+        //     $table->integer('personal_info_id');
+        //     $table->boolean('is_active')->default(1);
+        //     $table->string('remember_token', 100)->nullable();
+        //     $table->timestamps();
+        // });
     }
 
     /**
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,7 +33,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(UsersTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
         $this->call(DefaultRolePermissionSeeder::class);
 //        $this->call(RolesTableSeeder::class);
         $this->call(SettingTableSeeder::class);
