481b915ec05a92b8c94ac6bfdc46cbb2d5728a4c
waiyanphoneaant <wypa07@gmail.com>
2024-02-29 14:10:29 +0700

    location structure component change 7

--- app/View/Components/subLocationTreeComponent.php
+++ app/View/Components/subLocationTreeComponent.php
@@ -2,43 +2,35 @@
 
 namespace App\View\Components;
 
-use Closure;
 use Illuminate\View\Component;
-use Illuminate\Contracts\View\View;
-use App\Models\settings\businessLocation;
+use App\Models\settings\BusinessLocation;
 
-class subLocationTreeComponent extends Component
+class SubLocationTreeComponent extends Component
 {
-    /**
-     * Create a new component instance.
-     */
     public $locationId;
     public $level;
     public $locations;
-    public $color=[
-        'primary',
-        'dark',
-        'success',
-        'info',
-        'warning'
-    ];
-    public function __construct($location,$level=1)
-    {
-        $this->locationId=$location['id'];
-        $this->level=$level<4 ? $level :1;
-        $this->locations=businessLocation::select('id','name')
-                            ->where('parent_location_id',$this->locationId)->get()->toArray();
+    public $mainColor;
 
+    public function __construct($location, $level = 1)
+    {
+        $this->locationId = $location['id'];
+        $this->level = $level < 4 ? $level : 1;
+        $this->locations = BusinessLocation::select('id', 'name')
+            ->where('parent_location_id', $this->locationId)
+            ->get()
+            ->toArray();
+        $this->mainColor = $this->getMainColor();
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     */
-    public function render(): View|Closure|string
+    public function render()
     {
-        $locations=$this->locations;
-        $mainColor=$this->color[$this->level] ?? 'success';
-        $level=$this->level;
-        return view('components.subLocationTreeComponent',compact('locations','mainColor','level'));
+        return view('components.sub-location-tree-component');
+    }
+
+    private function getMainColor()
+    {
+        $colors = ['primary', 'dark', 'success', 'info', 'warning'];
+        return $colors[$this->level - 1] ?? 'success';
     }
 }
